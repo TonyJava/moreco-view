ba3943f4d5a0f842799f4cca2e2a6637e2e11910
wei.zhang <wei.zhang@dmall.com>
2018-11-17 18:07:02 +0800

    update

--- src/config/index.js
+++ src/config/index.js
@@ -14,7 +14,7 @@ export default {
    */
   baseUrl: {
     // dev: 'https://www.easy-mock.com/mock/5add9213ce4d0e69998a6f51/iview-admin/',
-    dev: 'http://localhost:8080/',
+    dev: ' https://www.easy-mock.com/mock/5beaa5a08eaaf00b3a4e1399/moreco/',
     pro: 'https://produce.com'
   },
   /**
