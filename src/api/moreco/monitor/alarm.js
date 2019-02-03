7326a35c634744ec6b1516da3de25cc083636207
wei.zhang <wei.zhang@dmall.com>
2019-02-03 13:18:03 +0800

    backup

---
+++ src/api/moreco/monitor/alarm.js
@@ -0,0 +1,36 @@
+export const page = () => {
+  return {
+    'ns': null,
+    'code': 0,
+    'msg': 'success',
+    'result': {
+      'currPage': 1,
+      'pageSize': 10,
+      'totalCount': 1,
+      'totalPage': 1,
+      'list': [
+        {
+          'id': 1,
+          'createdDate': null,
+          'createdBy': null,
+          'updatedDate': null,
+          'updatedBy': null,
+          'yn': 1,
+          'ruleCode': '监控系统',
+          'code': 'monitor'
+        },
+        {
+          'id': 2,
+          'createdDate': null,
+          'createdBy': null,
+          'updatedDate': null,
+          'updatedBy': null,
+          'yn': 1,
+          'name': '权限管理系统',
+          'code': 'rbac'
+        }
+      ]
+    },
+    'ext': {}
+  }
+}
---
+++ src/view/moreco/monitor/project-detail.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>detial</div>
+</template>
+<script>
+
+</script>
